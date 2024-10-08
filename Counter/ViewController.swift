e995062cb81e83997d59d355aeee547557d366ff
Dmitrii Bauzhadze <dmitry_bauzhadze@mail.ru>
2024-10-08 08:23:32 +0400

    Создан проект

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
-
+var mtCount = 0
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text: UITextView!
+    
+    @IBOutlet weak var buttonCount: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        text.text = "Счёт нажатий: \(mtCount)"
     }
 
-
+    @IBAction func Button1(_ sender: Any) {
+        mtCount += 1
+        text.text = "Счёт нажатий: \(mtCount)"
+    }
+    
 }
 
